0dcd5689343d2fa60f6cc22eb8e57cf8f766fcd0
Vasstin <vastin115408@gmail.com>
2021-12-08 22:06:17 +0200

    first step

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr className = "header">
+            <th>Название товара</th>
+            <th>Количество</th>
+            <th>Стоимость 1 ед.</th>
+            <th>Общая Стоимость</th>
+          </tr>
+        </thead>
+        <tbody className = 'body'>
+          <tr>
+            <td>Красный носок</td>
+            <td><input type = 'number'/></td>
+            <td><input  type = 'number'/></td>
+            <td><label>0</label></td>
+          </tr>
+          <tr>
+            <td>Синий носок</td>
+            <td><input type = 'number'/></td>
+            <td><input type = 'number'/></td>
+            <td><label>0</label></td>
+          </tr>
+          <tr>
+            <td>Бесцветный носок</td>
+            <td><input type = 'number'/></td>
+            <td><input type = 'number'/></td>
+            <td><label>0</label></td>
+          </tr>
+        </tbody>
+        <tfoot className = "footer">
+          <tr>
+            <td colSpan = '3'>Общая стоимость товаров</td>
+            <td><lable >0</lable></td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
   );
 }
